d6544778d470230923bd5635085b30f10fa070c2
Ben Wilde <Ben.Wilde@rosslyndatatech.com>
2022-09-08 13:36:46 +0100

    please work

--- src/utils/fetchRequest.js
+++ src/utils/fetchRequest.js
@@ -27,7 +27,7 @@ export const postComp = async (compName, compCode) => {
 export const getAll = async () => {
   try {
     const response = await fetch(
-      `https://langdon.azure-api.net/api/GetRequest/Get&apiKey=c28abe027f5d468cbedef72310dc06ee`,
+      `https://langdon.azure-api.net/api/GetRequest/Get`,
       {
         // method: "GET",
         headers: {
@@ -51,7 +51,7 @@ export const singIn = async (
 ) => {
   try {
     const response = await fetch(
-      `https://langdon.azure-api.net/api/Authentication/Login&apiKey=c28abe027f5d468cbedef72310dc06ee`,
+      `https://langdon.azure-api.net/api/Authentication/Login`,
       {
         // method: "POST",
         headers: {
